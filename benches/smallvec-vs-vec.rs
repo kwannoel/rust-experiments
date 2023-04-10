73d16665e575187dc5a049574c197e61157e0c8b
Noel Kwan <noelkwan1998@gmail.com>
2023-04-10 15:32:12 +0800

    add indexing benchmarks

--- benches/smallvec-vs-vec.rs
+++ benches/smallvec-vs-vec.rs
@@ -23,6 +23,16 @@ fn bench_smallvec(c: &mut Criterion) {
         v.push(black_box(i as u8));
     }
     }, BatchSize::SmallInput));
+        c.bench_function("smallvec indexing", |b| b.iter_batched(|| {
+        let mut v: SmallVec<[u8; 32]> = black_box(SmallVec::with_capacity(64 * 4));
+        for i in 0..32 {
+            v.push(black_box(i as u8));
+        }
+        v
+    }, |mut v| { for i in 0..32 {
+        black_box(v[i]);
+    }
+    }, BatchSize::SmallInput));
 }
 
 fn bench_vec(c: &mut Criterion) {
@@ -33,6 +43,15 @@ fn bench_vec(c: &mut Criterion) {
         v.push(black_box(i as u8));
     }
     }, BatchSize::SmallInput));
+    c.bench_function("vec indexing", |b| b.iter_batched(|| {
+        let mut v: Vec<u8> = black_box(vec![123; 32]);
+        for i in 0..32 {
+            v.push(black_box(i as u8));
+        }
+        v
+    }, |v| { for i in 0..32 {
+        black_box(v[i]);
+    }}, BatchSize::SmallInput));
 }
 
 criterion_group!(benches, bench_smallvec, bench_vec, bench_array);
