a9f83013ba32fd02f122f9a5ae8deccfe456ee88
Noel Kwan <noelkwan1998@gmail.com>
2023-04-10 15:19:01 +0800

    add various array insert benchmarks

---
+++ benches/smallvec-vs-vec.rs
@@ -0,0 +1,39 @@
+//! Benchmark 4 word op on 64 bit arch.
+
+use criterion::BatchSize;
+use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::black_box;
+use smallvec::SmallVec;
+
+fn bench_array(c: &mut Criterion) {
+     c.bench_function("array inserts", |b| b.iter_batched(|| {
+        let v: [u8; 32] = black_box([0; 32]);
+        v
+    }, |mut v| { for i in 0..32 {
+         v[i] = black_box(i as u8);
+    }
+    }, BatchSize::SmallInput));
+}
+
+fn bench_smallvec(c: &mut Criterion) {
+    c.bench_function("smallvec inserts", |b| b.iter_batched(|| {
+        let v: SmallVec<[u8; 32]> = black_box(SmallVec::with_capacity(64 * 4));
+        v
+    }, |mut v| { for i in 0..32 {
+        v.push(black_box(i as u8));
+    }
+    }, BatchSize::SmallInput));
+}
+
+fn bench_vec(c: &mut Criterion) {
+    c.bench_function("vec inserts", |b| b.iter_batched(|| {
+        let v: Vec<u8> = black_box(Vec::with_capacity(64 * 4));
+        v
+    }, |mut v| { for i in 0..32 {
+        v.push(black_box(i as u8));
+    }
+    }, BatchSize::SmallInput));
+}
+
+criterion_group!(benches, bench_smallvec, bench_vec, bench_array);
+criterion_main!(benches);
