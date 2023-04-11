5b918871b831dd9aec808285db16285d2bf17fa9
Noel Kwan <noelkwan1998@gmail.com>
2023-04-11 13:14:19 +0800

    bench indexed writes

--- benches/smallvec-vs-vec.rs
+++ benches/smallvec-vs-vec.rs
@@ -1,22 +1,37 @@
 //! Benchmark 4 word op on 64 bit arch.
 
-use criterion::black_box;
 use criterion::BatchSize;
-use criterion::{criterion_group, criterion_main, Criterion};
-use smallvec::{SmallVec, smallvec};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::ops::IndexMut;
+use smallvec::{SmallVec as LibSmallVec, smallvec};
+
+type SmallVec = LibSmallVec<[u8; 16]>;
+
+fn build_small_vec() -> SmallVec {
+    let sz = black_box(16);
+    black_box(smallvec![0; sz])
+}
+
+fn build_vec() -> Vec<u8> {
+    let sz = black_box(16);
+    black_box(vec![0; sz])
+}
+
+fn build_array() -> [u8; 16] {
+    black_box([0; 16])
+}
+
+fn bench_indexed_writes(mut v: impl AsMut<[u8]> + IndexMut<usize, Output=u8>) {
+    for i in 0..16 {
+        v[i] = black_box(16-(i as u8));
+    }
+}
 
 fn bench_array(c: &mut Criterion) {
-    c.bench_function("array inserts", |b| {
+    c.bench_function("array indexed writes", |b| {
         b.iter_batched(
-            || {
-                let v: [u8; 16] = black_box([0; 16]);
-                v
-            },
-            |mut v| {
-                for i in 0..16 {
-                    v[i] = black_box(i as u8);
-                }
-            },
+            build_array,
+            bench_indexed_writes,
             BatchSize::SmallInput,
         )
     });
@@ -26,7 +41,7 @@ fn bench_smallvec(c: &mut Criterion) {
     c.bench_function("smallvec inserts", |b| {
         b.iter_batched(
             || {
-                let v: SmallVec<[u8; 16]> = black_box(SmallVec::with_capacity(16));
+                let v: SmallVec = black_box(SmallVec::with_capacity(16));
                 v
             },
             |mut v| {
@@ -40,7 +55,7 @@ fn bench_smallvec(c: &mut Criterion) {
     c.bench_function("smallvec indexing", |b| {
         b.iter_batched(
             || {
-                let v: SmallVec<[u8; 16]> = black_box(smallvec![123; 16]);
+                let v: SmallVec = black_box(smallvec![123; 16]);
                 v
             },
             |v| {
@@ -48,6 +63,13 @@ fn bench_smallvec(c: &mut Criterion) {
                     black_box(v[i]);
                 }
             },
+            BatchSize::SmallInput,
+        )
+    });
+    c.bench_function("smallvec indexed writes", |b| {
+        b.iter_batched(
+            build_small_vec,
+            bench_indexed_writes,
             BatchSize::SmallInput,
         )
     });
@@ -79,6 +101,13 @@ fn bench_vec(c: &mut Criterion) {
                     black_box(v[i]);
                 }
             },
+            BatchSize::SmallInput,
+        )
+    });
+    c.bench_function("vec indexed writes", |b| {
+        b.iter_batched(
+            build_vec,
+            bench_indexed_writes,
             BatchSize::SmallInput,
         )
     });
