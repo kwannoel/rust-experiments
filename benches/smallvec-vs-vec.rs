4fad91bdff2ed48e4d92e005bee7c761314a6036
Noel Kwan <noelkwan1998@gmail.com>
2023-04-11 17:07:06 +0800

    bench on u8

--- benches/smallvec-vs-vec.rs
+++ benches/smallvec-vs-vec.rs
@@ -3,48 +3,50 @@
 use criterion::BatchSize;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::ops::{Index, IndexMut};
-use smallvec::{SmallVec as LibSmallVec, smallvec};
+// use smallvec::{SmallVec as LibSmallVec, smallvec};
+use smallvec::{SmallVec as LibSmallVec, smallvec, smallvec_inline};
 use paste::paste;
 use std::ops::DerefMut;
+use dhat;
+use dhat::Profiler;
 
-type SmallVec = LibSmallVec<[u64; 16]>;
+type SmallVec = LibSmallVec<[u8; 16]>;
 
 // ========== Build various datatypes =================
 
 fn build_smallvec() -> SmallVec {
-    let sz = black_box(16);
-    black_box(smallvec![0; sz])
+    // black_box(smallvec![0; 16])
+    black_box(smallvec_inline![0; 16])
 }
 
-fn build_vec() -> Vec<u64> {
-    let sz = black_box(16);
-    black_box(vec![0; sz])
+fn build_vec() -> Vec<u8> {
+    black_box(vec![0; 16])
 }
 
-fn build_array() -> [u64; 16] {
+fn build_array() -> [u8; 16] {
     black_box([0; 16])
 }
 
 // ========== Benchmark methods =================
 
 /// Benchmark index op.
-fn bench_index(v: impl Index<usize, Output=u64>) {
+fn bench_index(v: impl Index<usize, Output=u8>) {
     for i in 0..10_000 {
         black_box(v[black_box(i%16)]);
     }
 }
 
 /// Benchmark assign at index.
-fn bench_indexed_writes(mut v: impl AsMut<[u64]> + IndexMut<usize, Output=u64>) {
+fn bench_indexed_writes(mut v: impl AsMut<[u8]> + IndexMut<usize, Output=u8>) {
     for i in 0..10_000 {
-        v[black_box(i%16)] = black_box(i as u64);
+        v[black_box(i%16)] = black_box(i as u8);
     }
 }
 
 /// Benchmark copy slice
-fn bench_copy_slice(mut v: impl DerefMut<Target=[u64]>) {
+fn bench_copy_slice(mut v: impl DerefMut<Target=[u8]>) {
     for i in 0..10_000 {
-        let slice = black_box([(i % 16) as u64; 16]);
+        let slice = black_box([(i % 16) as u8; 16]);
         v[..(black_box(16))].copy_from_slice(&slice[..]);
     }
 }
@@ -54,7 +56,7 @@ macro_rules! bench_inserts {
     () => {
         |mut v| {
             for i in 0..16 {
-                v.push(black_box(i as u64));
+                v.push(black_box(i as u8));
             }
         }
     }
@@ -103,10 +105,12 @@ fn bench_vec(c: &mut Criterion) {
 }
 
 criterion_group!(benches, bench_smallvec, bench_vec, bench_array);
-// #[cfg(feature = "dhat-heap")]
-// #[global_allocator]
-// static ALLOC: dhat::Alloc = dhat::Alloc;
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
 fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
     benches();
     Criterion::default()
         .configure_from_args()
